1bd3e83be08038b4c2118dfa9c7226525dd238ff
Zoe Chow <zoechowym22@gmail.com>
2024-11-25 19:20:36 -0500

    dojo walls

--- sketch.js
+++ sketch.js
@@ -10,13 +10,57 @@ function setup() {
         scaleX: 1000,
         scaleY: 1000,
         asset: 'floorMat',
-        repeatX: 100,
-        repeatY: 100,
+        repeatX: 150,
+        repeatY: 150,
         rotationX: -90
     });
 
     world.add(ground);
     world.setFlying(false);
+
+    for (let i = 0; i < 4; i++) {
+        let xPos; 
+        let zPos; 
+        let angle;
+
+        switch(i) {
+            case 0:
+                xPos = 0;
+                zPos = 50;
+                angle = 0;
+            break;
+
+            case 1:
+                xPos = 0;
+                zPos = -50;
+                angle = 0;
+            break;
+
+            case 2:
+                xPos = 50;
+                zPos = 0;
+                angle = 90;
+            break;
+
+            case 3:
+                xPos = -50;
+                zPos = 0;
+                angle = -90;
+            break;
+        }
+
+        let wall = new AFrameP5.Plane({
+            x:xPos, y: 1, z: zPos,
+            scaleX: 100, scaleY: 50,
+            asset: 'wall',
+            repeatX: 3,
+            repeatY: 3,
+            side: "double",
+            rotationY: angle
+        })
+    
+        world.add(wall);
+    }
 
 }
 
