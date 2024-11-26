b570d568f78e1486d799063adc56b345b6c26310
Zoe Chow <zoechowym22@gmail.com>
2024-11-25 19:39:26 -0500

    table and placeholder fruits

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 
 function setup() {
 
+    //creating the dojo environment
     noCanvas();
 
+    //getting ref to VR scene
     world = new AFrameP5.World('VRScene');
 
+    //making the ground
     let ground = new AFrameP5.Plane({
         x: 0, y: -2, z:0,
         scaleX: 1000,
@@ -14,10 +17,12 @@ function setup() {
         repeatY: 150,
         rotationX: -90
     });
-
     world.add(ground);
+
+    //no flying
     world.setFlying(false);
 
+    //making the dojo walls
     for (let i = 0; i < 4; i++) {
         let xPos; 
         let zPos; 
@@ -50,7 +55,7 @@ function setup() {
         }
 
         let wall = new AFrameP5.Plane({
-            x:xPos, y: 1, z: zPos,
+            x:xPos, y: 1, z: zPos - 30,
             scaleX: 100, scaleY: 50,
             asset: 'wall',
             repeatX: 3,
@@ -62,8 +67,9 @@ function setup() {
         world.add(wall);
     }
 
+    //making dojo ceiling
     let ceiling = new AFrameP5.Plane({
-        x:0, y: 25, z: 0,
+        x:0, y: 25, z: -30,
         scaleX: 100, scaleY: 100,
         asset: 'ceiling',
         repeatX: 10,
@@ -71,8 +77,31 @@ function setup() {
         side: "double",
         rotationX: -90
     })
-
     world.add(ceiling);
+
+    //making tutorial table
+    let table = new AFrameP5.Box({
+        x: 0, y:-1.5, z: -3,
+        scaleX: 10,
+        scaleY: 1,
+        scaleZ: 2,
+        red: 150,
+        green: 75,
+        blue: 0
+    });
+    world.add(table);
+
+    //placeholder fruits
+    for (let i = 0; i < 5; i++) {
+        let fruit = new AFrameP5.Sphere({
+            x: -4 + i*2, y: -0.5, z: -3,
+            scaleX: random(0.3, 0.8), scaleY: random(0.3, 0.8), scaleZ: random(0.3, 0.8),
+            red: random(0, 256),
+            green: random(0, 256),
+            blue: random(0, 256)
+        });
+        world.add(fruit);
+    }
 
 }
 
