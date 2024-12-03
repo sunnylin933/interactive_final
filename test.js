29ececad588c1615ac1284300e1d8b9225829077
Sunny L. <94069216+sunnylin933@users.noreply.github.com>
2024-12-02 23:28:39 -0500

    Test 3

--- test.js
+++ test.js
@@ -26,8 +26,19 @@ function setup() {
     //Katana
     katana = new AFrameP5.GLTF({
         asset: 'katana',
-        x: 0, y: 0, z: 0,
+        x: 0, y: 0, z: 2,
         scaleX: 1, scaleY: 1, scaleZ: 1,
     });
     world.add(katana);
+
+    LHand = document.querySelector('#leftHand');
+}
+
+function draw(){
+    // Not sure why this isn't working. No nudge class for LHand. 
+    // Probably because its not actually a part of the Aframe P5 library, 
+    // but then how do I dynamically update the position?
+    //LHand.nudge(0,0.1,0);
+    
+    katana.nudge(0,0.1,0);
 }
